00e306583b594635cb64b04c3fa2259c35950356
jdjulius <julio.david.velasquez@gmail.com>
2020-02-19 15:28:09 -0600

    muestra de menu de opciones realizado

--- src/main/java/laboratoriofinal/CPLaboratorioFinal.java
+++ src/main/java/laboratoriofinal/CPLaboratorioFinal.java
@@ -14,12 +14,40 @@ import negocio.CatalogoPeliculas;
  */
 public class CPLaboratorioFinal {
 
-    Scanner scanner;
-    int opcion;
-    String nombre;
+    static Scanner scanner;
+    static int opcion;
+    static String nombre;
     CatalogoPeliculas catalogoPeliculas;
 
     public static void main(String[] args) {
 
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Elije opcion:");
+            System.out.println("1.- Iniciar catalogo peliculas");
+            System.out.println("2.- Agregar peliculas");
+            System.out.println("3.- Buscar pelicula");
+            System.out.println("0.- Salir");
+
+            opcion = Integer.parseInt(scanner.nextLine());
+
+            if (opcion == 1) {
+                System.out.println("opcion1");
+
+            } else if (opcion == 2) {
+                System.out.println("opcion2");
+
+            } else if (opcion == 3) {
+                System.out.println("opcion3");
+
+            } else if (opcion == 0) {
+                System.out.println("opcion0");
+
+                break;
+            }
+        }
+
+        System.out.println("TERMINADO");
     }
 }
